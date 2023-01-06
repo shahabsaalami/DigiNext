48810d88da945a3b9e06501341f23effcaeadbf9
Shahab Saalami <Shab.salami@gmail.com>
2023-01-06 22:58:08 +0330

    answer first question

--- src/main/kotlin/First.kt
+++ src/main/kotlin/First.kt
@@ -1,7 +1,30 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    first("abcdrf",50)
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+/**
+ * Answer of first question
+ */
+private fun first( str:String,count :Int) : Int {
+
+    var countOfRepeatA = 0
+    if (count<=0 || str.isEmpty()){
+        println("Invalid Value")
+    return countOfRepeatA
+    }
+
+    var newString = ""
+    while (newString.length <= count){
+        newString+=str
+    }
+    newString= newString.subSequence(0,count).toString()
+    newString.forEach {
+        if (it == 'a'){
+            countOfRepeatA++
+        }
+    }
+    println("newStringSize=$newString size=${newString.length} \n countOfRepeat=$countOfRepeatA")
+
+    return countOfRepeatA
+
 }
